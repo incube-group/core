51971ce5411143f27f23843bb23820fa17c982c6
Leszek Andzel <leszek.andzel@incubegroup.com>
2018-07-25 14:50:19 +0200

    PreconditionsTest.cs added

---
+++ test/InCube.Core.Test/PreconditionsTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using static InCube.Core.Preconditions;
+
+namespace InCube.Core.Test
+{
+    class PreconditionsTest
+    {
+        [Test]
+        public void TestCheckArgument()
+        {
+            var expression = true;
+            var errorMessageTemplate = "expression was {}";
+            CheckArgument(expression, errorMessageTemplate, expression);
+            expression = false;
+            Assert.Throws<ArgumentException>(() => CheckArgument(expression, errorMessageTemplate, expression),
+                "expression was {0}", expression);
+            var test = String.Format("template {0}", false);
+        }
+    }
+}
