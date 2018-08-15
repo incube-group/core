a8db698f8a66c8412bbf46d1f4398d9e069e6a71
Martin Marciniszyn Mehringer <martin.mehringer@incubegroup.com>
2018-08-15 18:12:01 +0200

    Extend tuples and introduce Functions.Invoke

--- src/InCube.Core/Collections/Tuples.cs
+++ src/InCube.Core/Collections/Tuples.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using JetBrains.Annotations;
 
 namespace InCube.Core.Collections
 {
@@ -9,32 +10,40 @@ namespace InCube.Core.Collections
     /// </summary>
     public static class Tuples
     {
+        [PublicAPI]
         public static (T2, T2) Select<T1, T2>(this (T1, T1) self, Func<T1, T2> functor) =>
             (functor(self.Item1), functor(self.Item2));
 
+        [PublicAPI]
         public static (T2, T2, T2) Select<T1, T2>(this (T1, T1, T1) self, Func<T1, T2> functor) =>
             (functor(self.Item1), functor(self.Item2), functor(self.Item3));
 
+        [PublicAPI]
         public static KeyValuePair<TK, TV> MakePair<TK, TV>(TK key, TV value) =>
             new KeyValuePair<TK, TV>(key, value);
 
+        [PublicAPI]
         public static Tuple<T1, T2> MakeTuple<T1, T2>(T1 item1, T2 item2)
             => new Tuple<T1, T2>(item1, item2);
 
+        [PublicAPI]
         public static (T1, T2) MakeValueTuple<T1, T2>(T1 item1, T2 item2)
             => (item1, item2);
 
+        [PublicAPI]
         public static IEnumerable<(T1, T2)> ZipAsTuple<T1, T2>(this IEnumerable<T1> left, IEnumerable<T2> right)
         {
             return left.Zip(right, MakeValueTuple);
         }
 
+        [PublicAPI]
         public static IEnumerable<(T1, T2, T3)> ZipAsTuple<T1, T2, T3>(this IEnumerable<T1> e1, IEnumerable<T2> e2,
             IEnumerable<T3> e3)
         {
             return e1.ZipAsTuple(e2).Zip(e3, (x, y) => (x.Item1, x.Item2, y));
         }
 
+        [PublicAPI]
         public static IEnumerable<(T1, T2, T3, T4)> ZipAsTuple<T1, T2, T3, T4>(this IEnumerable<T1> e1,
             IEnumerable<T2> e2,
             IEnumerable<T3> e3,
@@ -43,6 +52,7 @@ namespace InCube.Core.Collections
             return e1.ZipAsTuple(e2, e3).Zip(e4, (x, y) => (x.Item1, x.Item2, x.Item3, y));
         }
 
+        [PublicAPI]
         public static IEnumerable<(T1, T2, T3, T4, T5)> ZipAsTuple<T1, T2, T3, T4, T5>(this IEnumerable<T1> e1,
             IEnumerable<T2> e2,
             IEnumerable<T3> e3,
@@ -52,28 +62,60 @@ namespace InCube.Core.Collections
             return e1.ZipAsTuple(e2, e3, e4).Zip(e5, (x, y) => (x.Item1, x.Item2, x.Item3, x.Item4, y));
         }
 
+        [PublicAPI]
         public static IEnumerable<U> TupleSelect<K, V, U>(this IEnumerable<(K Key, V Value)> enumerable,
             Func<K, V, U> mapper) =>
             enumerable.Select(kv => mapper(kv.Key, kv.Value));
 
+        [PublicAPI]
         public static IEnumerable<U> TupleSelect<K, V, U>(this IEnumerable<KeyValuePair<K, V>> enumerable,
             Func<K, V, U> mapper) =>
             enumerable.Select(kv => mapper(kv.Key, kv.Value));
 
+        [PublicAPI]
         public static IEnumerable<(T value, int index)> ZipWithIndex<T>(this IEnumerable<T> enumerable) =>
             enumerable.Select(MakeValueTuple);
 
+        [PublicAPI]
         public static IEnumerable<KeyValuePair<T, V>> MapValues<T, U, V>(
             this IEnumerable<KeyValuePair<T, U>> enumerable, Func<U, V> mapper) =>
-            enumerable.Select(keyValue => Tuples.MakePair(keyValue.Key, mapper(keyValue.Value)));
+            enumerable.Select(keyValue => MakePair(keyValue.Key, mapper(keyValue.Value)));
 
+        [PublicAPI]
         public static IEnumerable<(T Key, V Value)> MapValues<T, U, V>(this IEnumerable<(T Key, U Value)> enumerable,
             Func<U, V> mapper) =>
             enumerable.Select(kv => (kv.Key, mapper(kv.Value)));
 
+        [PublicAPI]
+        public static IEnumerable<V> Values<T, V>(
+            this IEnumerable<(T Key, V Value)> enumerable) =>
+            enumerable.TupleSelect((_, value) => value);
+
+        [PublicAPI]
+        public static IEnumerable<V> Values<T, V>(
+            this IEnumerable<KeyValuePair<T, V>> enumerable) =>
+            enumerable.TupleSelect((_, value) => value);
+
+        [PublicAPI]
+        public static IEnumerable<T> Keys<T, V>(
+            this IEnumerable<KeyValuePair<T, V>> enumerable) =>
+            enumerable.TupleSelect((key, _) => key);
+
+        [PublicAPI]
+        public static IEnumerable<T> Keys<T, V>(
+            this IEnumerable<(T Key, V Value)> enumerable) =>
+            enumerable.TupleSelect((key, _) => key);
+
+        [PublicAPI]
         public static IEnumerable<(T, V)> AsTuple<T, V>(this IEnumerable<KeyValuePair<T, V>> enumerable)
         {
-            return enumerable.Select(kv => (kv.Key, kv.Value));
+            return enumerable.TupleSelect((key, value) => (key, value));
+        }
+
+        [PublicAPI]
+        public static IEnumerable<KeyValuePair<T, V>> AsKeyValuePair<T, V>(this IEnumerable<(T Key, V Value)> enumerable)
+        {
+            return enumerable.TupleSelect(MakePair);
         }
 
     }
---
+++ src/InCube.Core/Functional/Functions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace InCube.Core.Functional
+{
+    /// <summary>
+    /// Helper for creating and applying lambda functions.
+    /// </summary>
+    public static class Functions
+    {
+        /// <summary>
+        /// Infers the return type of a functional expression.
+        /// </summary>
+        /// <typeparam name="T">type being inferred</typeparam>
+        /// <param name="func">a function to evaluate</param>
+        /// <returns></returns>
+        [PublicAPI]
+        public static T Invoke<T>(Func<T> func) => func();
+    }
+}
