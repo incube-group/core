135b31628bdb029e315708213ba805da7370ce48
julien <julien.debache@incubegroup.com>
2021-03-03 11:16:59 +0100

    [NOISSUE] Added two small array utilities.

---
+++ src/InCube.Core/Collections/Arrays.cs
@@ -0,0 +1,38 @@
+﻿using JetBrains.Annotations;
+
+namespace InCube.Core.Collections
+{
+    /// <summary>
+    /// A collection of extension methods related to arrays.
+    /// </summary>
+    [PublicAPI]
+    public static class Arrays
+    {
+        /// <summary>
+        /// Creates a constant array.
+        /// </summary>
+        /// <param name="length">The legnth of the outpout array.</param>
+        /// <param name="value">The value to set at every index.</param>
+        /// <typeparam name="T">The type of the output array.</typeparam>
+        /// <returns>An array filled with a single object.</returns>
+        public static T[] Constant<T>(this int length, T value)
+            where T : notnull
+        {
+            var result = new T[length];
+            result.Set(value);
+            return result;
+        }
+
+        /// <summary>
+        /// Sets all the elements of the <paramref name="array"/> to the <see cref="value"/>.
+        /// </summary>
+        /// <param name="array">The array to modify.</param>
+        /// <param name="value">The value to modify the array with.</param>
+        /// <typeparam name="T">The type of the array.</typeparam>
+        public static void Set<T>(this T[] array, T value)
+        {
+            for (var i = 0; i < array.Length; i++)
+                array[i] = value;
+        }
+    }
+}
---
+++ test/InCube.Core.Test/Collections/ArraysTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using InCube.Core.Collections;
+using NUnit.Framework;
+
+namespace InCube.Core.Test.Collections
+{
+    public class ArraysTest
+    {
+        [Test]
+        public void Test_Set_EmptyArray()
+        {
+            var empty = Array.Empty<object>();
+            empty.Set(new object());
+            Assert.IsEmpty(empty);
+        }
+
+        [Test]
+        public void Test_Set_ReferenceEqualityBetweenMembers()
+        {
+            var array = new object[2];
+            array.Set(new object());
+            Assert.AreEqual(array[0], array[1]);
+        }
+
+        [Test]
+        public void Test_Constant_ZeroLength_Empty()
+        {
+            var result = 0.Constant(new object());
+            Assert.IsEmpty(result);
+        }
+
+        [Test]
+        public void Test_Constant_ReferenceEqualityBetweenMembers()
+        {
+            var result = 2.Constant(new object());
+            Assert.AreEqual(result[0], result[1]);
+
+        }
+    }
+}
