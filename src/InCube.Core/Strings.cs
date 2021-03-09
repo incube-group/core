16d915fc6974c501126c4df880799e2d545b3afc
julien <julien.debache@incubegroup.com>
2021-03-09 16:01:12 +0100

    [NOISSUE] Added small string utility.

---
+++ src/InCube.Core/Strings.cs
@@ -0,0 +1,16 @@
+﻿namespace InCube.Core
+{
+    /// <summary>
+    /// A collection of extension methods aimed at <see cref="string"/>s.
+    /// </summary>
+    public static class Strings
+    {
+        /// <summary>
+        /// Returns the <paramref name="alternative"/> in case <paramref name="self"/> is null or white space.
+        /// </summary>
+        /// <param name="self">The string to replace with an alternative in case it is null or white space.</param>
+        /// <param name="alternative">The alternative to use as replacement in case <paramref name="self"/> is null or white space.</param>
+        /// <returns>A nullable <see cref="string"/>.</returns>
+        public static string? OrElseIfNullOrWhiteSpace(this string? @self, string? alternative) => string.IsNullOrWhiteSpace(@self) ? alternative : @self;
+    }
+}
---
+++ test/InCube.Core.Test/StringsTest.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+
+namespace InCube.Core.Test
+{
+    public class StringsTest
+    {
+        [Test]
+        public void Test_NotNullOrWhiteSpace_NotReplaced()
+        {
+            const string? original = "a";
+            var result = original.OrElseIfNullOrWhiteSpace("b");
+            Assert.AreEqual(original, result);
+        }
+
+        [Test]
+        public void Test_IsNull_IsReplaced()
+        {
+            string? original = null;
+            const string? alternative = "b";
+            var result = original.OrElseIfNullOrWhiteSpace(alternative);
+            Assert.AreEqual(alternative, result);
+        }
+
+        [Test]
+        public void Test_IsWhiteSpace_IsReplaced()
+        {
+            const string? original = "";
+            const string? alternative = "b";
+            var result = original.OrElseIfNullOrWhiteSpace(alternative);
+            Assert.AreEqual(alternative, result);
+        }
+
+        [Test]
+        public void Test_Idempotence()
+        {
+            const string? original = "";
+            const string? alternative = "b";
+            var result = original.OrElseIfNullOrWhiteSpace(alternative).OrElseIfNullOrWhiteSpace("c");
+            Assert.AreEqual(alternative, result);
+        }
+    }
+}
