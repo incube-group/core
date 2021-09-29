1a4a66a6531f32af1bea067e9b0165006d1d0438
julien <julien.debache@incubegroup.com>
2021-09-29 13:17:00 +0200

    [NOISSUE] First attempt at IAsyncEnumerables

---
+++ src/InCube.Core/Collections/AsyncEnumerables.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using JetBrains.Annotations;
+
+namespace InCube.Core.Collections
+{
+    /// <summary>
+    /// A collection of extension methods to work with <see cref="IAsyncEnumerable{T}"/>.
+    /// </summary>
+    [PublicAPI]
+    public static class AsyncEnumerables
+    {
+        /// <summary>
+        /// Chunks an <see cref="IAsyncEnumerable{T}"/> into an <see cref="IAsyncEnumerable{T}"/> of lists.
+        /// </summary>
+        /// <param name="self">An <see cref="IAsyncEnumerable{T}"/>.</param>
+        /// <param name="chunkSize">The size of the chunks.</param>
+        /// <typeparam name="T">The type of collection.</typeparam>
+        /// <returns>An <see cref="IAsyncEnumerable{T}"/> of lists.</returns>
+        public static async IAsyncEnumerable<IReadOnlyList<T>> Chunk<T>(this IAsyncEnumerable<T> self, int chunkSize)
+            where T : class
+        {
+            if (chunkSize <= 0)
+                throw new ArgumentException($"Chunksize {chunkSize} is not supported.", nameof(chunkSize));
+            var buffer = new T[chunkSize];
+            var counter = 0;
+            await foreach (var t in self)
+            {
+                if (counter == chunkSize)
+                {
+                    yield return buffer.ToImmutableList();
+                    buffer.Set(null);
+                    counter = 0;
+                }
+
+                buffer[counter] = t;
+                ++counter;
+            }
+
+            if (counter != 0)
+                yield return buffer[..counter].ToImmutableList();
+        }
+    }
+}
---
+++ test/InCube.Core.Test/Collections/AsyncEnumerablesTest.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using InCube.Core.Collections;
+using NUnit.Framework;
+
+namespace InCube.Core.Test.Collections
+{
+    public class AsyncEnumerablesTest
+    {
+        [Test]
+        public async Task Test_Empty()
+        {
+            var looped = false;
+            await foreach (var l in EnumerateObjects(0).Chunk(100))
+            {
+                Assert.IsFalse(looped);
+                Assert.IsEmpty(l);
+                looped = true;
+            }
+        }
+
+        [Test]
+        public async Task Test_Single()
+        {
+            var looped = false;
+            await foreach (var l in EnumerateObjects(1).Chunk(100))
+            {
+                Assert.IsFalse(looped);
+                Assert.AreEqual(1, l.Count);
+                looped = true;
+            }
+        }
+
+        [Test]
+        public async Task Test_TwoChunks()
+        {
+            var loopCount = 0;
+            await foreach (var l in EnumerateObjects(200).Chunk(100))
+            {
+                Assert.AreEqual(100, l.Count);
+                loopCount++;
+            }
+            Assert.AreEqual(2, loopCount);
+        }
+
+        private static async IAsyncEnumerable<object> EnumerateObjects(int howMany)
+        {
+            for (var i = 0; i < howMany; i++)
+            {
+                yield return new object();
+            }
+        }
+    }
+}
